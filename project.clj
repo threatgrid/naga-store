8925b6eb9c77be6f486729bf01d96f4171273468
rubygeek <nola@rubygeek.com>
2019-04-08 14:07:20 -0500

    fix for github url to show up on clojars page

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject org.clojars.quoll/naga-store "0.3.0"
   :description "Protocol library for Naga storage"
+  :url "https://github.com/threatgrid/naga-store"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.520"]
                  [prismatic/schema "1.1.10"]]

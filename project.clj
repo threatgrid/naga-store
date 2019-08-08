58deb96ee616ca1f62f0980a3153695f74cc3e8d
Paula Gearon <pgearon@cisco.com>
2019-08-08 15:39:54 -0400

    bumped to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.quoll/naga-store "0.3.2"
+(defproject org.clojars.quoll/naga-store "0.3.2-SNAPSHOT"
   :description "Protocol library for Naga storage"
   :url "https://github.com/threatgrid/naga-store"
   :license {:name "Eclipse Public License"

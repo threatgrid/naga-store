73ceae3692bff1124d0dddaa3bbf6da7b2ab62e4
Paula Gearon <pgearon@cisco.com>
2019-11-27 23:01:33 -0500

    Updated dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.quoll/naga-store "0.3.3-SNAPSHOT"
+(defproject org.clojars.quoll/naga-store "0.3.4"
   :description "Protocol library for Naga storage"
   :url "https://github.com/threatgrid/naga-store"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.520"]
-                 [prismatic/schema "1.1.10"]]
+                 [org.clojure/clojurescript "1.10.597"]
+                 [prismatic/schema "1.1.12"]]
   :plugins [[lein-cljsbuild "1.1.7"]]
   :cljsbuild {
     :builds {
